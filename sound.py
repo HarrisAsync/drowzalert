56907096ca327e191217c5317f6d73256557b24f
sssskipjack <jack.zheng.nz@gmail.com>
2023-03-04 15:50:53 +1100

    added multiple messages

--- sound.py
+++ sound.py
@@ -2,9 +2,26 @@ from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
 import tempfile
-import pygame
+import pygame 
+from random import randint
+
 
 class Sound_Alerts:
+    def __init__(self):
+        self.drowsy_list = [
+        "Our system has detected signs of drowsiness. It's important to take a break and rest before continuing your journey for your own safety.",
+        "We've noticed indications of fatigue. Please take a moment to rest before continuing your journey for your well-being.",
+        "Alert: You appear to be feeling drowsy. We recommend taking a break and resting before continuing your journey for your own safety.",
+        "Your driving seems to indicate fatigue. Please consider taking a break and getting some rest before continuing your journey for your safety.",
+        "Attention: Signs of drowsiness have been detected. For your safety, we suggest taking a break and resting before resuming your journey."
+        ]
+        self.sleep_list = [
+        "It seems like your eyes may be closed. For your safety and the safety of others, please pull over to a safe location immediately and take a break from driving.",
+        "To ensure your safety and the safety of those around you, please immediately pull over to a secure location and take a break from driving if it appears that your eyes are closed.",
+        "If it seems like your eyes are closed, please stop driving and take a break in a secure area to ensure your safety and the safety of others.",
+        "In order to guarantee your own safety and that of other individuals on the road, if you think your eyes are closed, please pull over to a secure location right away and take a break from driving.",
+        "To avoid any risks and keep yourself and others safe, please pull over to a safe spot immediately and take a break from driving if it seems like your eyes may be closed."
+        ]
     
     """
     has one function: input
@@ -16,14 +33,20 @@ class Sound_Alerts:
     # 1: User seems drowsy
     # 2: user has fallen asleep
     # 3: 
+
         pygame.mixer.init()
+        
+        index = randint(0, 4)
+
         
         if input == 1:
             chime = pygame.mixer.Sound("dunDun.wav")
             chime.play()
-            self.create_sound("Drowsiness detected. Please consider taking a break and rest before continuing your journey.")
+            self.create_sound(self.drowsy_list[index])
         elif input == 2:
-            self.create_sound("You may be falling asleep. It's time to take a break and rest before continuing your journey. Please pull over to a safe location and take a break.")
+            chime = pygame.mixer.Sound("Alarm.wav")
+            chime.play()
+            self.create_sound(self.sleep_list[index])
         elif input == 3:
             chime = pygame.mixer.Sound("AUGHHHH.wav")
             chime.play()
